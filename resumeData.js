ad8349f7cea8e74fe4389ff615f68fc867042a14
Lu Mono <lu.mono@klarna.com>
2025-07-19 22:36:40 +0200

    feat: update resume data

--- resumeData.js
+++ resumeData.js
@@ -63,10 +63,19 @@ const resumeData = {
                     "managed_budget": "Approx. €4M annually",
                     "tech_stack": ["Python (Pandas, NumPy)", "ApacheAirflow", "SQL (Athena)", "AWS", "Apache Kafka", "Qliksense", "Quicksight", "Jenkins"],
                     "description": [
-                        "Managed and mentored teams of analysts, engineers, and data scientists across Sweden, Germany, Spain, and Poland.",
-                        "Oversaw implementation and operation of AML systems, integrating AI-driven detection across >10M transactions/month.",
-                        "Led system upgrades, analyzed system data to identify financial crime trends, and utilized advanced data science techniques to enhance detection algorithms.",
-                        "Ensured compliance with AML regulations and managed projects for system implementation and upgrades."
+                        "Manage and mentor two teams, the BAU team and the Project team, consisting of analysts, engineers, SMEs, data scientists, and managers.",
+                        "Foster a culture of continuous improvement and technical proficiency.",
+                        "Oversee the configuration and operation of AML systems, aligning them with current regulatory requirements and best practices.",
+                        "Lead system upgrades and work with IT and vendors to enhance performance, ensuring comprehensive monitoring capabilities.",
+                        "Analyze system-generated data to identify trends or anomalies related to potential financial crimes and prepare detailed reports for senior management and regulatory reviews.",
+                        "Utilize advanced data science techniques, including machine learning and predictive modeling, to enhance detection algorithms and improve system accuracy.",
+                        "Collaborate with data scientists to develop and implement analytical models that detect emerging patterns and adapt to new money laundering tactics.",
+                        "Ensure data integrity and leverage big data tools to manage and analyze large data sets efficiently.",
+                        "Ensure policies and procedures comply with AML regulations, adapting systems as needed to maintain up-to-date compliance.",
+                        "Coordinate with compliance, risk management, IT, and internal audit teams for system alignment and improvement.",
+                        "Engage with regulators and auditors during compliance reviews.",
+                        "Assist investigations by providing relevant data and insights and implement ad hoc system checks when unusual activities are detected.",
+                        "Oversee projects related to the implementation and upgrading of AML systems, ensuring timely and budget-conscious completion."
                     ],
                     "achievements": [
                         "20% reduction in operational workload and 12% reduction in average handling time",
@@ -84,9 +93,22 @@ const resumeData = {
                     "team_locations": ["Vietnam", "Singapore", "India", "Philippines"],
                     "managed_budget": "USD 2M annually",
                     "tech_stack": ["SpringBoot", "Docker", "Kubernetes", "AWS", "Python", "Node.js", "Terraform", "GitHub Actions", "Azure", "MySQL"],
-                    "description": ["Built and coached a team of 20+ members across Vietnam, Singapore, and India."],
+                    "description": 
+                    [
+                        "Take ownership of and develop the company's technical strategy to support growth ambitions as well as short and long term business objectives.",
+                        "Lead and manage a team of highly experienced technology professionals, incorporating teams of Software Engineers, Test Engineers, DevOps and Architects.",
+                        "Work alongside other senior leaders to coordinate and deliver a seamless and effective end-to-end engagement with clients, with a particular focus on developing strong and successful approaches for cross-functional teams and design into delivery.",
+                        "Be accountable for the quality and delivery of all technical output, engendering others to take ownership and pride aligned to a great customer experience.",
+                        "Identify opportunities to add value to the company and clients through appropriate new technologies, and be able to develop business cases to support ideas.",
+                        "Build and maintain a strong industry reputation and ensure the implementation of market leading agile engineering practices and processes, with a clear plan for developing people to support this objective.",
+                        "Evangelize technology within the business, build a strong community of motivated, highly engaged professionals and bring the C-suite on this journey.",
+                        "Identify the right skills, team structure and resources to grow the technology teams in local markets, consisting of primarily permanent resource as well as contractors and vendors.",
+                        "Drive forward key engineering initiatives such as DevOps/release management improvements, standardized tooling and common metrics/monitoring capabilities.",
+                        "Lead the charge for a secure by design and quality first engineering function owning standards around code testing and automation."
+                    ],
                     "achievements": [
-                        "Restructured development environments, reducing product development time from 3 months to 3 days."
+                        "Restructured chaotic development environments by standardizing processes, tech stacks, and infrastructure.",
+                        "Innovated insurance product development methods, reducing product development time from 3 months to 3 days"
                     ]
                 },
                 {
@@ -99,10 +121,22 @@ const resumeData = {
                     "team_locations": ["Vietnam", "Singapore", "Philippines"],
                     "managed_budget": "USD 5M annually",
                     "tech_stack": ["Java", "SpringBoot", "SQL", "Jenkins", "Xcode"],
-                    "description": ["Managed 6 vendor teams with a total of 100+ members across Vietnam, Singapore, and the Philippines."],
+                    "description": 
+                    [
+                        "Be responsible for project technical implementation for all countries in the Region for Delivery Services projects, ensuring projects are delivered with a focus on scope, quality, timeliness and within budget.",
+                        "Maintain business alignment across work streams in country, by working closely with senior stakeholders to maintain scope and expectation.",
+                        "Be responsible for managing the cross-functional teams at a country level and ensure alignment back up to the project level, including business & technical teams (Business Analysts, Architects, Delivery Teams, UX/UI Designers, Testers) and also across the Manulife organization.",
+                        "Ensure that the release planning and transition to BAU is completed at the country level to determine a clear roadmap for functionality delivered in line with the End State Vision for the country.",
+                        "Provide project oversight to ensure that the plan and business scope for solution delivery is maintained as per the business case developed.",
+                        "Act as the point of contact for the country to advise any operational in-efficiencies and enhancement requests for the solutions being deployed at the country level.",
+                        "Responsible to ensure that the solution being implemented at the country level is leveraging the best practice across the countries in the region.",
+                        "Enforce the digital solutions vision for best practices and integrate these elements to create the optimal customer experience.",
+                        "Work with POS, PMS, BAs, Architecture team, and others to translate business ideas and functional requirements into high-level design, low-level design and technical requirements for development projects including middleware integrations.",
+                        "Introduce effective software development practices and methodologies aimed at controlling costs and delivering quality solutions on a timely basis while building a strong, well-motivated development team.",
+                        "Practice influence, negotiation, and collaboration to work across the enterprise globally and with third-party vendors."
+                    ],
                     "achievements": [
-                        "Delivered 35+ insurance products within a 10-month deadline and introduced solutions that reduced development and testing efforts by 10x.",
-                        "Managed 6 vendor teams with a total of 100+ members across Vietnam, Singapore, and the Philippines."
+                        "Delivered 35+ insurance products within a 10-month deadline and introduced solutions that reduced development and testing efforts by 10x."
                     ]
                 },
                 {
@@ -114,11 +148,51 @@ const resumeData = {
                     "market_served": ["Global"],
                     "team_locations": ["Singapore"],
                     "managed_budget": "N/A",
-                    "tech_stack": ["Cucumber BDD","Selenium Webdriver", "Jenkins", "Python", "Shell Scripting"],
-                    "description": [
-                        "Automated 90+ applications with Jenkins pipelines, reducing processing time by 50%."
+                    "tech_stack": ["Cucumber BDD","Selenium Webdriver", "Jenkins", "Python", "Shell Scripting", "Jira", "Confluence"],
+                    "description": 
+                    [
+                        "Understand problems from the customer perspective and implement automation strategies based on issues found on deployed systems.",
+                        "Coordinate with the development team to ensure UI guidelines and standards are followed.",
+                        "Identify opportunities for automation in all areas of testing.",
+                        "Set up and conduct reviews at various points in the development cycle to ensure the product is meeting design specifications and quality standards.",
+                        "Conduct acceptance tests that include testing of features as well as reliability and performance testing.",
+                        "Work with PM, Dev and other QA engineers during creation of test case writing, planning and execution.",
+                        "Effectively prioritize and execute tasks in a challenging, fast paced, global environment.",
+                        "Work independently, in team settings, and in multicultural/multi-region environments."
                     ],
-                    "achievements": []
+                    "achievements": [
+                        "Set up the automation framework for the company's products, reducing manual testing efforts by 50%."
+                    ]
+                },
+                {
+                    "title": "Lead System Analyst",
+                    "company": "National University of Singapore",
+                    "website": "http://www.nus.edu.sg",
+                    "location": "Singapore",
+                    "dates": "2017-05 to 2017-11",
+                    "market_served": ["Singapore"],
+                    "team_locations": ["Singapore"],
+                    "managed_budget": "N/A",
+                    "tech_stack": ["Java", "SpringBoot", "SQL", "Jenkins", "JSP", "HTML", "CSS", "JavaScript"],
+                    "description": [
+                        "Conduct technical feasibility study, analyse functional specifications and collaborate with teams to design and architect solutions including developing proof of concepts.",
+                        "Lead high quality technical designs and solutions that will meet user's requirement.",
+                        "Provide technical mentoring on various technical issues, identify technical risks and ensure compliance with security, development and quality standards.",
+                        "Design and implement complex modules and interfaces while supervising and reviewing junior developers' work and technical deliverables.",
+                        "Document and update the technical design specifications.",
+                        "Prepare effort estimation, develop technical activities, schedule and work breakdown structures to ensure compliance to project scope, deliverables, resources and budget.",
+                        "Identify and keep abreast of latest application development and technology trends on new initiative and emerging technologies.",
+                        "Conduct or facilitate knowledge sharing of emerging technologies.",
+                        "Provide post-implementation maintenance support encountered with the existing applications.",
+                        "Provide assistance to business users to facilitate their business needs.",
+                        "Adhere to ISO processes to comply with department's security, quality and ISO audit requirements.",
+                        "Supervise and manage a team of developers.",
+                        "Provide technical guidance and mentorship to junior team members during design, implementation and testing phase."
+                    ],
+                    "achievements": [
+                        "Automated 90+ applications with Jenkins pipelines, reducing processing time by 50%.",
+                        "Streamlined workflows with an automated approval system."
+                    ]
                 },
                 {
                     "title": "System Analyst, IT Sales Support",
@@ -130,9 +204,29 @@ const resumeData = {
                     "team_locations": ["Singapore"],
                     "managed_budget": "N/A",
                     "tech_stack": ["Java", "Mobile Development (Android/iOS)", "Groovy", "Gradle", "Angular", "SpringBoot", "Jenkins"],
-                    "description": [],
+                    "description": [
+                        "Lead or collaborate with technical teams - train, retain and supervise full-time and/or contract software developers and other technical staff as necessary.",
+                        "Provide leadership and direction to ensure software developers are working effectively and successfully at completing tasks.",
+                        "Assist in preparation of project plans, monitor project progress, and report status to stakeholders and management.",
+                        "Provide project-specific and client-specific information processing services to the business.",
+                        "Maintain, build and release plans for new functionality and enhancements.",
+                        "Plan work, assign resources, identify issues and resolve conflicts.",
+                        "Establish and manage procedures for reviewing, estimating, and accepting development work requests.",
+                        "Work with Project Managers, Business Analysts, Solutions Architects and others to translate business ideas and functional requirements into high-level design, low-level design and technical requirements for development projects including middleware integrations.",
+                        "Coordinate and participate in the analysis, design, development, unit testing and maintenance of mission-critical business applications.",
+                        "Participate in R&D activities.",
+                        "Introduce effective software development practices and methodologies aimed at controlling costs and delivering quality solutions on a timely basis while building a strong, well-motivated development team.",
+                        "Represent development teams at partner and management meetings.",
+                        "Work with subcontractors and business partners to ensure completion of agreed activities.",
+                        "Liaise with clients to ensure successful implementation of new initiatives.",
+                        "Participate in special projects and ad hoc analyses as necessary.",
+                        "Remain abreast of relevant technology developments.",
+                        "Other duties as assigned."
+                    ],
                     "achievements": [
-                        "Redesigned engine applications for mobile offline computation and achieved a 50% reduction in product development man-days."
+                        "Redesigned engine applications for mobile offline computation, enhancing user accessibility.",
+                        "Achieved a 50% reduction in product development man-days through reusable common engines.",
+                        "Established automated testing and deployment pipelines, ensuring high-quality deliverables."
                     ]
                 }
             ]
